ed4bcd2c7c7c98bd7bdfa502de0fb53063b32cd2
Hiroki Ando <hikuohiku@gmail.com>
2023-12-11 22:07:41 +0900

    chapter07-initial

---
+++ hiro/chapter07/1/script.jsx
@@ -0,0 +1,23 @@
+
+const app = document.getElementById("app")
+
+function Header({ title }) {
+  return <h1>{title ? title : 'Default Title'}</h1>;
+}
+
+function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<HomePage />, app);
