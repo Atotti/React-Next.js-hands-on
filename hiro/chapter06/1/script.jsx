976be29b6dcd856cff0f6dbd36f9bfd398fcb0a5
Hiroki Ando <hikuohiku@gmail.com>
2023-12-11 17:23:51 +0900

    chapter06 initial commit

---
+++ hiro/chapter06/1/script.jsx
@@ -0,0 +1,16 @@
+
+const app = document.getElementById("app")
+
+function Header() {
+  return <h1>Develop. Preview. Ship. 🚀</h1>;
+}
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+ReactDOM.render(<HomePage />, app);
